58f9655442776f570ce23264763df68dfaa54bda
Lata789 <85116291+Lata789@users.noreply.github.com>
2022-02-14 15:16:44 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,2 @@
+<br><br>
+<div class="footer"> © 2021 Gold Plate Restaurant Developed By : Team 52</div>
---
+++ admin/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="style2.css">
+<div class="h">Gold Plate Restaurant</div>
+<div class="mbg">
+	<a href="after_login.php" class="menu">Parcel Clients</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="food.php" class="menu">Food Menu</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="review.php" class="menu">Reviews</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="food_cat.php" class="menu">Food Category</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="logout.php" class="menu" style="color: #ff0157;">Logout</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	
+</div>
+<br><br>
